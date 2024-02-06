a5e4739bb9572b116f8ff5653fabbeb7df7d8336
qdenys <khrapov.denys.js@gmail.com>
2024-02-06 16:08:45 +0200

    Ignored all .log files and modified README.md

---
+++ main.js
@@ -0,0 +1,33 @@
+// console.log('main.js loaded');
+
+
+$(document).ready(function () {
+	'use strict';
+
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+
+
+	// var c;
+	//
+	// for (var x = 25; x < 400; x += 50) {
+	// 	for (var y = 25; y < 400; y += 50) {
+	// 		c = Shape.Circle(x, y, 20);
+	// 		c.fillColor = 'green';
+	// 	}
+	// }
+
+	var tool = new Tool();
+	var c = Shape.Circle(200, 200, 80);
+	c.fillColor = 'black';
+	var text = new PointText(200, 200);
+	text.justification = 'center';
+	text.fillColor = 'white';
+	text.fontSize = 20;
+	text.content = 'hello world';
+	tool.onMouseDown = function (e) {
+		var c = Shape.Circle(e.point, 20);
+		c.fillColor = 'green';
+	};
+	paper.view.draw();
+});
